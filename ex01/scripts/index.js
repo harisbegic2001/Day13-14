3bd7b7caa39032425c56f3ae4a9ab22e3dc1e1d5
Haris Begić <harisbegic01@hotmail.com>
2021-03-21 19:17:38 +0100

    Final changes 2nd project

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,31 +1,22 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-var imgBlock = document.getElementById("img-block");
+var iframeBlock = document.getElementById("iframe-block");
+var imgTitle = document.getElementsByClassName("img-title");
 
-// Get the button that opens the modal
-var gridItem = document.getElementsByClassName("grid-item");
-
-for(i = 0; i < gridItem.length; i++){
-  gridItem[i].addEventListener('click', function(){
-    var imgFull = this.getAttribute('data-image');
+for (i = 0; i < imgTitle.length; i++) {
+  imgTitle[i].addEventListener("click", function () {
+    var iframeTitle = this.getAttribute("data-iframe");
 
     modal.style.display = "block";
-    imgBlock.innerHTML = '<img class="w-100" src="../images/' + imgFull + '">';
+    iframeBlock.innerHTML =
+      '<iframe src="' + iframeTitle + '" width="100%" height="500"></iframe>';
   });
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 };
